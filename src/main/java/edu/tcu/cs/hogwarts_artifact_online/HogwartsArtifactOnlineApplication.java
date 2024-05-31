2fc03792de8057a269cfbd56f3e5e56928cd352c
Kuldeep Singh <kd@Kuldeeps-MacBook-Air.local>
2024-05-31 15:32:44 +0530

    This is the first commit

---
+++ src/main/java/edu/tcu/cs/hogwarts_artifact_online/HogwartsArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwarts_artifact_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtifactOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwarts_artifact_online/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwarts_artifact_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
